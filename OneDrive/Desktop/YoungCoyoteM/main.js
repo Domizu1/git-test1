5b1275060a0ddb3f9f5ed2a222a4def807c0bc97
Domizu1 <savadumancic2@gmail.com>
2025-09-03 17:13:37 +0200

    123

---
+++ OneDrive/Desktop/YoungCoyoteM/main.js
@@ -0,0 +1,34 @@
+
+ScrollReveal().reveal('.map-section', {
+  origin: 'left',
+  distance: '100px',
+  duration: 1600,
+  delay: 200,
+  easing: 'ease-out',
+  opacity: 0,
+  reset: false
+});
+ScrollReveal().reveal('.offer-card', {
+  origin: 'bottom',
+  distance: '50px',
+  duration: 1200,
+  interval: 200,
+  opacity: 0,
+  reset: false
+});
+ScrollReveal().reveal('.hero-text', {
+  origin: 'top',
+  distance: '60px',
+  duration: 1500,
+  delay: 300,
+  opacity: 0
+});
+ScrollReveal().reveal('.contact-card', {
+  origin: 'right',
+  distance: '100px',
+  duration: 1600,
+  delay: 200,
+  easing: 'ease-out',
+  opacity: 0,
+  reset: false
+});
